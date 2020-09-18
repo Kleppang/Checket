bbaa6e14c8927ff02fa2533a49b29a6d0e37e1c7
RK-USN <226996@usn.no>
2020-09-18 15:07:04 +0200

    Firebase implementert, authentication implementert

--- app/src/main/java/no/checket/checket/MainActivity.java
+++ app/src/main/java/no/checket/checket/MainActivity.java
@@ -2,15 +2,47 @@ package no.checket.checket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import no.checket.checket.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Lmao", "TesT");
+
+        // Initializing the FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null) {
+            // User is logged in
+            //updateUI(currentUser);
+        } else {
+            // User is not currently logged in, prompt
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+    }
+
+    public void startLogin(View view) {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
