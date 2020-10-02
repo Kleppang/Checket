dda9411265d649349d33a3f582f9473b682b0dad
Coolstream <Coolstream@mail.com>
2020-10-02 14:02:41 +0200

    onMenuItemClick for Tasks og Achievements i BottomAppDrawer onClick for FAB

--- app/src/main/java/no/checket/checket/MainActivity.java
+++ app/src/main/java/no/checket/checket/MainActivity.java
@@ -1,8 +1,10 @@
 package no.checket.checket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Jasså, åpne Navigation Drawer ja", Toast.LENGTH_SHORT).show();
             }
         });
+
+        main_BottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.main_BottomAppBar_tasks:
+                        // Open tasks
+                        Toast.makeText(MainActivity.this, "Open tasks", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.main_BottomAppBar_ach:
+                        // Open achievements
+                        Toast.makeText(MainActivity.this, "Open achievements", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
+    public void newTask(View view) {
+        // User has clicked the FAB
+        Toast.makeText(this, "Create a new task", Toast.LENGTH_SHORT).show();
+    }
 }
