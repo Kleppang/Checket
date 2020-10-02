511dbf15abd1aab8a8026d3b521073371a6f4ac4
RK-USN <226996@usn.no>
2020-10-02 13:53:04 +0200

    Klargjort onClickListener til Navigation Drawer i MainActivity

--- app/src/main/java/no/checket/checket/MainActivity.java
+++ app/src/main/java/no/checket/checket/MainActivity.java
@@ -3,13 +3,28 @@ package no.checket.checket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomAppBar main_BottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        main_BottomAppBar = findViewById(R.id.main_BottomAppBar);
+
+        main_BottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Jasså, åpne Navigation Drawer ja", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
