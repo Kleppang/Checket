35dbf0071759baa8b46956aeaf0cfb4ea06d93e5
Coolstream <Coolstream@mail.com>
2020-10-17 18:58:06 +0200

    Tester på om bruker har skrevet noe, forbedret knapp under autentisering

--- app/src/main/java/no/checket/checket/LoginRegisterActivity.java
+++ app/src/main/java/no/checket/checket/LoginRegisterActivity.java
@@ -38,9 +38,6 @@ public class LoginRegisterActivity extends AppCompatActivity {
     }
 
     public void loginUser(View view) {
-        btn_login = findViewById(R.id.btn_login);
-        btn_login.setClickable(false);
-        btn_login.setText(R.string.LoginRegisterActivity_wait);
 
         eTxt_email = findViewById(R.id.inp_login_email);
         eTxt_password = findViewById(R.id.inp_login_pw);
@@ -48,30 +45,44 @@ public class LoginRegisterActivity extends AppCompatActivity {
         String email = eTxt_email.getText().toString();
         String password = eTxt_password.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail: success");
-                    //FirebaseUser user = mAuth.getCurrentUser();
+        if(!email.isEmpty() && !password.isEmpty()) {
+            btn_login = findViewById(R.id.btn_login);
 
-                    // User authenticated, onStart's mAuth.getCurrentUser() will now return this user
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail: failure", task.getException());
-                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+            btn_login.setClickable(false);
+            btn_login.setText(R.string.LoginRegisterActivity_wait);
+            btn_login.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            btn_login.setTextColor(getResources().getColor(R.color.colorDarkGray));
 
-                    // Clear password
-                    eTxt_password.setText("");
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail: success");
+                        //FirebaseUser user = mAuth.getCurrentUser();
 
-                    btn_login.setClickable(true);
-                    btn_login.setText(R.string.LoginRegisterActivity_login);
+                        // User authenticated, onStart's mAuth.getCurrentUser() will now return this user
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail: failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                        // Clear password
+                        eTxt_password.setText("");
+
+                        btn_login.setClickable(true);
+                        btn_login.setText(R.string.LoginRegisterActivity_login);
+
+                        btn_login.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                        btn_login.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Please fill out both fields.", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
