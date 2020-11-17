e291a6b6d96eb74ce255cc94ce6b7b782d8a16ee
FetterPiskvik <petterfisk@gmail.com>
2020-11-17 18:02:08 +0100

    Dialog
    Dialogelementer på plass, mangler funksjonalitet for lagrin av nytt objekt med denne dataen.

--- app/src/main/java/no/checket/checket/NewTaskFragment.java
+++ app/src/main/java/no/checket/checket/NewTaskFragment.java
@@ -1,14 +1,22 @@
 package no.checket.checket;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
 
 public class NewTaskFragment extends DialogFragment {
     @NonNull
@@ -24,17 +32,59 @@ public class NewTaskFragment extends DialogFragment {
         // Adding buttons for saving or aborting
             .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
+                public void onClick(DialogInterface dialog, int i) {
                     // TODO: save as an object, add to data structure
                 }
             })
             .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
+                public void onClick(DialogInterface dialog, int i) {
                     NewTaskFragment.this.getDialog().cancel();
                 }
             });
 
         return builder.create();
+    }
+
+
+    // Inner class to handle the time picker
+    public static class TimePickerFragment extends DialogFragment
+            implements android.app.TimePickerDialog.OnTimeSetListener {
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Setting the default values to the current time
+            final Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker View, int hour, int minute) {
+            // TODO: Do something useful. What with the returning and such
+        }
+    }
+
+    // Inner class to similarly handle the date picker
+    public static class DatePickerFragment extends DialogFragment
+            implements android.app.DatePickerDialog.OnDateSetListener {
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use current date as default
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of the date picker
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            // TODO: Do something useful. What with the returning and such
+        }
     }
 }
