a0aeb7094f1aedcdc3ea200fec2a740be3a42f11
Coolstream <Coolstream@mail.com>
2020-11-17 15:59:20 +0100

    Henter achievements fra Firestore, sjekker om UID matcher innlogget bruker sin UID

--- app/src/main/java/no/checket/checket/AchievementsActivity.java
+++ app/src/main/java/no/checket/checket/AchievementsActivity.java
@@ -1,22 +1,92 @@
 package no.checket.checket;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AchievementsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    // Firebase, declare instance of Firestore and Auth
+    private FirebaseFirestore firestore;
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "AchievementsActivity";
+
+    /* TODO:
+        As a user may still get achievements while not being logged in, connect list of achievements with local database
+
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
 
+        // Firebase, initialize the instance
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() == null) {
+            // User is not logged in, will show local achievements only, for now, kick back to where the user came from
+            onBackPressed();
+            Toast.makeText(this, "Not logged in", Toast.LENGTH_LONG).show();
+        }
+
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle(R.string.achievements);
         }
+
+        final List<Achievement> achList = new ArrayList<>();
+        final AchievementRecAdapter achAdapter = new AchievementRecAdapter(achList);
+
+        recyclerView = findViewById(R.id.achievements_recView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(achAdapter);
+
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("achievements").addSnapshotListener(new EventListener<QuerySnapshot>() {
+
+            @Override
+            public void onEvent(@Nullable QuerySnapshot documents, @Nullable FirebaseFirestoreException err) {
+                if(err == null) {
+                    for(DocumentChange thisDoc:documents.getDocumentChanges()) {
+                        if(thisDoc.getType() == DocumentChange.Type.ADDED) {
+                            // Check if the UID matches logged in users' UID
+                            if(thisDoc.getDocument().getString("uid").equals(mAuth.getCurrentUser().getUid())) {
+                                Achievement newAchievement = new Achievement(thisDoc.getDocument().getString("name"), thisDoc.getDocument().getString("desc"));
+
+                                achList.add(newAchievement);
+                                achAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                } else {
+                    // If an error occurred
+                    Log.e(TAG, "Error occurred: " + err.getMessage());
+                }
+            }
+
+        });
     }
 
 
