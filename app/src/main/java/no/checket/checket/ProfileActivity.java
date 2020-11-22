16f5680a20365ec7b95010b8daef4a498e46d761
mrbajro <49783343+mrbajro@users.noreply.github.com>
2020-11-22 18:04:53 +0100

    Work in progress med visning av Brukernavn i profil

--- app/src/main/java/no/checket/checket/ProfileActivity.java
+++ app/src/main/java/no/checket/checket/ProfileActivity.java
@@ -8,16 +8,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProfileActivity extends AppCompatActivity {
     ImageView callEditprofile;
+
+    private TextView txtV_username;
 
     // Firebase, declare instance of Firestore and Auth
     private FirebaseFirestore firestore;
@@ -44,6 +48,21 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+/*        firestore.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for (QueryDocumentSnapshot thisDoc : task.getResult()) {
+                        // Check if the UID matches logged in users' UID
+                        if(thisDoc.getString("uid").equals(mAuth.getCurrentUser().getUid())) {
+                            txtV_username = findViewById(R.id.profile_username);
+                            txtV_username.setText(thisDoc.getString("name"));
+                        }
+                    }
+                }
+            }
+        });
+*/
     }
 
     @Override
@@ -51,5 +70,5 @@ public class ProfileActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
-    
+
 }
