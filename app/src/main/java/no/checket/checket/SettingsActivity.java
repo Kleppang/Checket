c669c6b461dd259c665ee2aba24e03bbc19ff5a4
Coolstream <Coolstream@mail.com>
2020-12-11 20:03:13 +0100

    Fjernet mye ubrukt import i SettingsActivity

--- app/src/main/java/no/checket/checket/SettingsActivity.java
+++ app/src/main/java/no/checket/checket/SettingsActivity.java
@@ -2,19 +2,9 @@ package no.checket.checket;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceManager;
-
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    private SettingsFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
